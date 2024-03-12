835e4a4488e35e9301ae1f3b760a01da7dab3032
c04m3 <h5n1711711@gmail.com>
2024-03-13 02:42:06 +0800

    🏵️ favicon & og:img fixes

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://og-image-craigary.vercel.app/**2033**%20TOWN.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-light-bg.svg', // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: '/about',
   seo: {
     keywords: ['Blog', 'Notion', '2033'],
