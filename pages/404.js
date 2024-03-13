a2bbfaddeb305635ce1aae401dde722f7957ff72
c04m3 <h5n1711711@gmail.com>
2024-03-14 05:52:00 +0800

    🥐 adjust content

--- pages/404.js
+++ pages/404.js
@@ -5,6 +5,7 @@ export default function Page404 () {
   const locale = useLocale()
 
   return (
+    // 新增 mt-6 mb-6 間距
     <Container>
       <h1 className="text-5xl text-black dark:text-white text-center mt-6 mb-6">404</h1>
       <p className="text-xl text-gray-600 dark:text-gray-300 text-center">{locale.PAGE.ERROR_404.MESSAGE}</p>
